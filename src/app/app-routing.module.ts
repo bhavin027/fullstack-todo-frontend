0460783dd81cda23c860ee26391e4a74c557f397
bhavin027 <bhavinghoghari27@gmail.com>
2020-12-01 19:56:05 +0530

    Implemented Log In and Log Out functionality using session in hardcoded authentication service

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { ErrorComponent } from './error/error.component';
 import { ListTodosComponent } from './list-todos/list-todos.component';
 import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 
 const routes: Routes = [
@@ -11,6 +12,8 @@ const routes: Routes = [
   { path: 'welcome', component: WelcomeComponent },
   { path: 'welcome/:urlParam', component: WelcomeComponent },
   { path: 'list-todos', component: ListTodosComponent },
+  { path: 'logout', component: LogoutComponent },
+  
   { path: '**', component: ErrorComponent }
 ];
 
--- src/app/service/hardcoded-authentication.service.ts
+++ src/app/service/hardcoded-authentication.service.ts
@@ -7,11 +7,25 @@ export class HardcodedAuthenticationService {
 
   constructor() { }
 
-  authenticate(username: String, password: String) {
+  authenticate(username: string, password: String) {
+
+    //console.log('Before login '+ this.isUserLoggedIn());
+
     if(username==="admin" && password==="test123") {
+      sessionStorage.setItem('authenticatedUser', username);
+      //console.log('after login '+ this.isUserLoggedIn());
       return true;
     }
     return false;
+  }
+
+  isUserLoggedIn() {
+    let user = sessionStorage.getItem('authenticatedUser');
+    return !(user===null);
+  }
+
+  logout () {
+    sessionStorage.removeItem('authenticatedUser');
   }
 
 }
